d8e199ea3b8a7cfedbef49780400d6b0c7a353dc
WilsonGoins <wilson.goins@ufl.edu>
2024-03-23 10:43:42 -0400

    added bootstrap

--- React/clubhub/src/App/App.js
+++ React/clubhub/src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import LandingPage from "../LandingPage/LandingPage"
+
 function App() {
     return (
         <BrowserRouter>
