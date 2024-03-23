bcca1ef6e5d2dd5216da11bc1246cac43590299f
WilsonGoins <wilson.goins@ufl.edu>
2024-03-23 10:29:46 -0400

    added react-router-dom and landing page

---
+++ React/clubhub/src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import LandingPage from "../LandingPage/LandingPage"
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact element={<LandingPage />} />
+                
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ React/clubhub/src/LandingPage/LandingPage.js
@@ -0,0 +1,7 @@
+const LandingPage = () => {
+    return (
+        <h1>ClubHub</h1>
+    )
+}
+
+export default LandingPage
